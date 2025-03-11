feb22619e4e442b53077dabc7b35a83fb3767f81
24LP2179 24LP2179 <24LP2179@G303-CC0147.local>
2025-03-11 13:06:58 +0800

    components

---
+++ src/app/components/header.tsx
@@ -0,0 +1,27 @@
+export const Header = () => {
+
+
+
+
+  return (
+    <div className="flex justify-between items-center h-[59px]">
+      <div className="flex">
+        <img src="film.png" />
+        <p className="text-[16px], text-[#4338CA] italic ml-2 font-bold leading-[20px] tracking-[0.32px]">
+          Movie Z
+        </p>
+      </div>
+      <div className="flex gap-3 items-center">
+        <div className="flex justify">
+          <img className="w-8 h-8 bg-white p-2 4 border rounded-md border-solid border-[#E4E4E7]" src="search.png" />
+          <div>
+            <input className="w-0 h-0" type="search"></input>
+          </div>
+        </div>
+        <div>
+          <img src="dark-mode.png" />
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,85 @@
-import Image from "next/image";
+"use client";
+import axios from "axios";
+import { ACCESS_TOKEN } from "./constants";
+import { useEffect, useState } from "react";
+import { Header } from "./components/header";
+import { MainPage } from "./components/MainPage";
+import { MovieList } from "./components/movielist";
+import { Footer } from "./components/Footer";
 
-export default function Home() {
+type Movie = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[]; // --> ni static type bichiglel. Array<number> gj bichvel generic type bichiglel
+  id: number;
+  original_language: string;
+  orignal_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
+export default function HOME() {
+  const [movieList, setMovieList] = useState([]);
+
+  const options = {
+    method: "GET",
+    url: "https://api.themoviedb.org/3/discover/movie",
+    params: {
+      include_adult: "false",
+      include_video: "false",
+      language: "en-US",
+      page: "1",
+      sort_by: "popularity.desc",
+    },
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${ACCESS_TOKEN}`,
+    },
+  };
+  const getMovies = async () => {
+    const movies = await axios.get(
+      "https://api.themoviedb.org/3/discover/movie",
+      {
+        headers: {
+          Authorization: `Bearer ${ACCESS_TOKEN}`,
+        },
+      }
+    );
+    console.log(movies);
+    setMovieList(movies.data.results); // haana ni result gdeg zuil bga ve??
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
-   <div>
-    
-   </div>
+    <div>
+      <Header></Header>
+      <div>
+        <MainPage />
+        <MovieList 
+        // onChange = {(string) => setMovieList([...movielist])}
+        // setMovieList={setMovieList}
+        // movieList = {movieList}
+        ></MovieList>
+        <Footer></Footer>
+      </div>
+
+  
+
+      {movieList.map((movie: Movie) => {
+        return (<div key={movie.title}>
+          <img className="w-[157px] " src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}/>
+          <p>{movie.vote_average}/10</p>
+          <h1>{movie.title}</h1>
+          </div>);
+      })}
+    </div>
   );
 }
